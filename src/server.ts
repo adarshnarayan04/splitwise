329498b5c3d1582a283848bd60dc45d49c4700cb
Adarsh Narayan <adarshnarayangumla@gmail.com>
2025-12-20 12:33:28 +0530

    add cors middleware and update dependencies in package.json

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import routes from "./routes/index.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import { dot } from "node:test/reporters";
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(routes);
